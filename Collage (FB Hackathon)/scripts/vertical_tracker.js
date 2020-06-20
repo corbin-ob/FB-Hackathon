738c2d5a5ba736a094e48d7a64fe9392048a3005
mogwai <feelirabs@gmail.com>
2020-06-20 02:06:00 +0100

    Added 3d object to placer patch

--- Collage (FB Hackathon)/scripts/vertical_tracker.js
+++ Collage (FB Hackathon)/scripts/vertical_tracker.js
@@ -3,15 +3,14 @@ const Diagnostics = require('Diagnostics');
 const FaceTracking = require('FaceTracking');
 
 const root = Scene.root;
-const VERTICAL_PLANE = 'VERTICAL_PLANE'
+const VERTICAL_PLANE = 'VERTICLANE'
 
 root.findFirst( 'planeTracker0', {recursive: true})
 .then(function(planetracker){
-    let tracker = planetracker.trackingMode;
-    tracker = VERTICAL_PLANE;
+    planetracker.trackingMode = VERTICAL_PLANE;
     let confidence = planetracker.confidence;
     const mouthOpennessValue = FaceTracking.face(0).mouth.openness;
-Diagnostics.watch('Checking plane tracking mode: ', tracker);
+Diagnostics.watch('Checking plane tracking mode: ', planetracker.trackingMode);
 Diagnostics.watch('Checking plane confidence level: ', confidence);
 Diagnostics.watch('Checking mouth openness: ', mouthOpennessValue)
 });
